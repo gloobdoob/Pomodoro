1712342f17a0346f18119ef5d815f56bf85a9b4c
gloobdoob <cvaalmeda@gmail.com>
2023-06-11 22:50:02 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,108 @@
+from tkinter import *
+
+import tkinter.ttk as ttk
+import math
+
+GREEN = '#90A17D'
+FONT_NAME = 'System'
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 10
+BG_COLOR = '#FFFFFF'
+reps = 0
+timer = None
+# ------ Function --------- #
+
+
+# reset
+
+def reset_timer():
+    global reps, timer
+
+    root.after_cancel(timer)
+    canvas.itemconfig(timer_text, text='00:00')
+    title_label.config(text='Pomodoro Timer', background= '#FFDE59',
+                       foreground='black', font=(FONT_NAME, 40, 'bold'))
+    reps = 0
+    start_btn['state'] = NORMAL
+
+
+# start timer
+
+
+def start_timer():
+    global reps, timer
+    start_btn['state'] = DISABLED
+
+    reps += 1
+    print(reps)
+
+    if reps % 2 == 0:
+        if reps == 8:
+            long_break_sec = LONG_BREAK_MIN * 60
+            title_label.config(text='Long Break', background='#FFDE59',
+                               foreground='black', font=(FONT_NAME, 40, 'bold'))
+            count_down(long_break_sec)
+            reps = 0
+        else:
+            shor_break_sec = SHORT_BREAK_MIN * 60
+            title_label.config(text='Short Break', background='#FFDE59',
+                               foreground='black', font=(FONT_NAME, 40, 'bold'))
+            count_down(shor_break_sec)
+    else:
+        title_label.config(text='Start working..', background='#FFDE59',
+                           foreground='black', font=(FONT_NAME, 40, 'bold'))
+        work_sec = WORK_MIN * 60
+        count_down(work_sec)
+
+
+# count down
+def count_down(count):
+    global reps, timer
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f'0{count_sec}'
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
+    if count > 0:
+        timer = root.after(1000, count_down, count-1)
+    else:
+        start_timer()
+
+
+# ------ UI SETUP --------- #
+root = Tk()
+root.title('POMODORO APP')
+root.geometry('780x700')
+root.resizable(False, False)
+root.config(background='#FFDE59', padx=45, pady=50)
+
+# Icon
+image_icon = PhotoImage(file='Timer_icon.png')
+root.iconphoto(False, image_icon)
+
+
+# Labels
+title_label = ttk.Label(text='Pomodoro Timer', background= '#FFDE59',
+                        foreground='black', font=(FONT_NAME, 40, 'bold'))
+title_label.grid(column=1, row=0)
+
+# Canvas setup
+canvas = Canvas(width=512, height=512, bg=BG_COLOR, highlightthickness=0)
+bg_image = PhotoImage(file='hehe.png')
+canvas.create_image(256, 256, image=bg_image)
+timer_text = canvas.create_text(
+    256, 350, text='00:00', fill='#FFFFFF', font=('MS Sans Serif', 80, 'bold'))
+canvas.grid(column=1, row=1)
+
+# Buttons
+start_btn = ttk.Button(text='Start',  command=start_timer)
+start_btn.grid(column=0, row=2)
+
+
+reset_btn = ttk.Button(text='Reset', command=reset_timer)
+reset_btn.grid(column=2, row=2)
+
+root.mainloop()
